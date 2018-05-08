398b814f347b2a05bcd804a5c90288abe4ed6334
pengxu <960135880@qq.com>
2018-05-08 12:31:15 +0000

    seed user and topic

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $updated_at = $faker->dateTimeThisMonth();
+
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
+    $sentence = $faker->sentence();
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
